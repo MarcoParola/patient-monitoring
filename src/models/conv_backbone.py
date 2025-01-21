22d834fe1ea5cac806db20977cfb71c925b7e540
Caterina Bruchi <81431058+caterinabruchi9@users.noreply.github.com>
2025-01-21 11:45:08 +0100

    Fix stride and padding

--- src/models/conv_backbone.py
+++ src/models/conv_backbone.py
@@ -8,27 +8,29 @@ class CNN3DLightning(pl.LightningModule):
         super(CNN3DLightning, self).__init__()
         self.learning_rate = learning_rate
         
-        # Block 1
-        self.conv1 = nn.Conv3d(in_channels=in_channels, out_channels=16, kernel_size=5, stride=1, padding=3)
-        self.pool1 = nn.MaxPool3d(kernel_size=5, stride=5, padding=2)
+         # Initialize the feature output dimension and dropout rate
+        self.feature_dim = 256
         self.dropout = nn.Dropout3d(p=0.1)
         
+        # Block 1
+        self.conv1 = nn.Conv3d(in_channels=in_channels, out_channels=16, kernel_size=7, stride=3, padding=3)
+        self.pool1 = nn.MaxPool3d(kernel_size=7, stride=3, padding=3)
+        
         # Block 2
-        self.conv2 = nn.Conv3d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2)
-        self.pool2 = nn.MaxPool3d(kernel_size=5, stride=5, padding=2)
+        self.conv2 = nn.Conv3d(in_channels=16, out_channels=32, kernel_size=5, stride=2, padding=2)
+        self.pool2 = nn.MaxPool3d(kernel_size=5, stride=2, padding=2)
         
         # Block 3
-        self.conv3 = nn.Conv3d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=2)
-        self.pool3 = nn.MaxPool3d(kernel_size=3, stride=3, padding=1)
+        self.conv3 = nn.Conv3d(in_channels=32, out_channels=32, kernel_size=5, stride=2, padding=2)
+        self.pool3 = nn.MaxPool3d(kernel_size=5, stride=2, padding=2)
         
         # Block 4
         self.conv4 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.pool4 = nn.MaxPool3d(kernel_size=5, stride=3, padding=1)
+        self.pool4 = nn.MaxPool3d(kernel_size=3, stride=1, padding=1)
         
-        # Output feature extractor
+        # Flatten the output and extract features
         self.flatten = nn.Flatten()
-        self.feature_dim = 64 * 2 * 2 # Adjusted to match final dimensions
-        self.feature_extractor = nn.Linear(self.feature_dim, 256)
+        self.feature_extractor = nn.Linear(256, self.feature_dim)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
