c405bcacc30e9cf453d17f3fd691bf1e34fa4f72
Caterina Bruchi <81431058+caterinabruchi9@users.noreply.github.com>
2025-01-27 09:49:50 +0100

    CNN Custom dataset training

--- bash_cate_CNN.sh
+++ bash_cate_CNN.sh
@@ -1,3 +1,12 @@
+
+
+#!/bin/bash
+
+# Configuration files
+CONFIG_FILE="config/config_cate.yaml"
+
+
+
 FPS_VALUES=(3)
 CAMERA_TYPES=(2 1 0)
 
@@ -5,10 +14,6 @@ CAMERA_TYPES=(2 1 0)
 CNN_CONV_BACKBONE="CNN3D"
 CNN_RESIZE_W=256
 CNN_RESIZE_H=256
-
-# Update patient_ids for train, val, and test in the config file
-    sed -i "s/^  patient_ids: .*/  patient_ids: [123,234]/" "$CONFIG_FILE"
-    sed -i "/^val:/,/^test:/s/^  patient_ids: .*/  patient_ids: [345]/" "$CONFIG_FILE"
 
 for FPS in "${FPS_VALUES[@]}"; do
   for CAMERA_TYPE in "${CAMERA_TYPES[@]}"; do
@@ -21,8 +26,25 @@ for FPS in "${FPS_VALUES[@]}"; do
     sed -i "s/^  fps: .*/  fps: $FPS/" "$CONFIG_FILE"
     sed -i "s/^  camera_type: .*/  camera_type: $CAMERA_TYPE/" "$CONFIG_FILE"
 
+        # Update patient_ids for train, val, and test in the config file
+    sed -i "/^train:/,/^val:/s/^  patient_ids: .*/  patient_ids: [123,234]/" "$CONFIG_FILE"
+    sed -i "/^val:/,/^test:/s/^  patient_ids: .*/  patient_ids: [345]/" "$CONFIG_FILE"
+    sed -i "/^test:/,/^$/s/^  patient_ids: .*/  patient_ids: [345]/" "$CONFIG_FILE"
+
+     # Add or update the 'end' and 'dataset' parameters
+    if ! grep -q "^end:" "$CONFIG_FILE"; then
+      echo "end: \"mlp\"" >> "$CONFIG_FILE"
+    else
+      sed -i "s/^end: .*/end: \"mlp\"/" "$CONFIG_FILE"
+    fi
+
+    if ! grep -q "^dataset:" "$CONFIG_FILE"; then
+      echo "dataset: \"Custom\"" >> "$CONFIG_FILE"
+    else
+      sed -i "s/^dataset: .*/dataset: \"Custom\"/" "$CONFIG_FILE"
+    fi
 
     # Execute the Python script for CNN trial
-    python train_cate.py
+    #python train_cate.py
   done
 done
