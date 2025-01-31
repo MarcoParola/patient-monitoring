78607708a26dd0add1735fa9711ae84c940c57a0
Caterina Bruchi <81431058+caterinabruchi9@users.noreply.github.com>
2025-01-31 14:57:08 +0100

    output_dim

--- CNN_KTH.sh
+++ CNN_KTH.sh
@@ -1,1 +1,1 @@
-python train_cate.py conv_backbone="CNN3D" end="mlp" dataset="KTH" KTH_dataset.fps=5 KTH_dataset.resize_w=160 KTH_dataset.resize_h=120 train.batch_size=8 train.max_epoch=200 train.lr=0.000001 KTH_dataset.path="dataset/KTH" KTH_dataset.csv_path="dataset/KTH_cut.csv"
+python train_cate.py conv_backbone="CNN3D" output_dim=6 end="mlp" dataset="KTH" KTH_dataset.fps=5 KTH_dataset.resize_w=160 KTH_dataset.resize_h=120 train.batch_size=8 train.max_epoch=200 train.lr=0.000001 KTH_dataset.path="dataset/KTH" KTH_dataset.csv_path="dataset/KTH_cut.csv"
