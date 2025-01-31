03879ca8486ec42c223a58eefe6681b405b154e5
Caterina Bruchi <81431058+caterinabruchi9@users.noreply.github.com>
2025-01-31 14:05:30 +0100

    new bash CNN

---
+++ CNN.sh
@@ -0,0 +1,2 @@
+
+python train_cate.py conv_backbone="CNN3D" end="mlp" dataset="KTH" KTH_dataset.fps=5 KTH_dataset.resize_w=160 KTH_dataset.resize_h=120 train.batch_size=8 train.max_epoch=200 train.lr=10-6 
