50fc5d22dfad41656085a1318ce887e71a766a55
Caterina Bruchi <81431058+caterinabruchi9@users.noreply.github.com>
2025-01-31 14:10:10 +0100

    lr fix

--- CNN_KTH.sh
+++ CNN_KTH.sh
@@ -1,2 +1,1 @@
-
-python train_cate.py conv_backbone="CNN3D" end="mlp" dataset="KTH" KTH_dataset.fps=5 KTH_dataset.resize_w=160 KTH_dataset.resize_h=120 train.batch_size=8 train.max_epoch=200 train.lr=10-6 
+python train_cate.py conv_backbone="CNN3D" end="mlp" dataset="KTH" KTH_dataset.fps=5 KTH_dataset.resize_w=160 KTH_dataset.resize_h=120 train.batch_size=8 train.max_epoch=200 train.lr=0.000001 KTH_dataset.path="dataset/KTH" KTH_dataset.csv_path="dataset/KTH_cut.csv"
