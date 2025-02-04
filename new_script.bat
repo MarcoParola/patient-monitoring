6cd99768de0a1327f99c0860f29b159840ad7e6b
BaffoBello14 <giuliobello12@gmail.com>
2025-02-04 10:41:06 +0100

    Aggiornato il dispositivo per l'addestramento e semplificato lo script di addestramento per il modello DEEP_PRIVACY2, includendo il parametro batch_size.

--- new_script.bat
+++ new_script.bat
@@ -1,17 +1,3 @@
-python train.py dataset="action" privacy_model_type="VIDEO_PRIVATIZER" pose_dataset.fps=5 privacy_attributes.skin_color=3 output_dim=19
-python train.py dataset="action" privacy_model_type="PIXELATE" pose_dataset.fps=5 privacy_attributes.skin_color=3 output_dim=19
-python train.py dataset="action" privacy_model_type="BLUR" pose_dataset.fps=5 privacy_attributes.skin_color=3 output_dim=19
 python train.py dataset="action" privacy_model_type="DEEP_PRIVACY2" pose_dataset.fps=5 privacy_attributes.skin_color=3 output_dim=19 train.batch_size=2
-python train.py dataset="action" privacy_model_type="STYLEGAN2" pose_dataset.fps=5 privacy_attributes.skin_color=3 output_dim=19
 
-python train.py privacy_model_type="VIDEO_PRIVATIZER" pose_dataset.fps=5
-python train.py privacy_model_type="PIXELATE" pose_dataset.fps=5
-python train.py privacy_model_type="BLUR" pose_dataset.fps=5
-python train.py privacy_model_type="DEEP_PRIVACY2" pose_dataset.fps=5 train.batch_size=2
-python train.py privacy_model_type="STYLEGAN2" pose_dataset.fps=5
-
-python train.py privacy_model_type="VIDEO_PRIVATIZER" pose_dataset.fps=5 train.camera_type=1 val.camera_type=1 test.camera_type=1
-python train.py privacy_model_type="PIXELATE" pose_dataset.fps=5 train.camera_type=1 val.camera_type=1 test.camera_type=1
-python train.py privacy_model_type="BLUR" pose_dataset.fps=5 train.camera_type=1 val.camera_type=1 test.camera_type=1
-python train.py privacy_model_type="DEEP_PRIVACY2" pose_dataset.fps=5 train.camera_type=1 val.camera_type=1 test.camera_type=1 train.batch_size=2
-python train.py privacy_model_type="STYLEGAN2" pose_dataset.fps=5 train.camera_type=1 val.camera_type=1 test.camera_type=1
+python train.py privacy_model_type="DEEP_PRIVACY2" pose_dataset.fps=5 train.camera_type=0 val.camera_type=0 test.camera_type=0 train.batch_size=2
